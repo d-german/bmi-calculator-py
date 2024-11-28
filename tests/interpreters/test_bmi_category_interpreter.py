5fcbd575ce0c5f36fc3cf5b6beebb3a3dea45a72
Damon German <damon.german@hyland.com>
2024-11-28 16:36:31 -0600

    Add new BMI and weight tests; remove obsolete student tests
    This commit introduces tests for BMI category interpretation and height model validation, ensuring coverage for valid and invalid inputs. Additionally, the outdated tests related to student repository functions have been removed to clean up the test suite.

---
+++ tests/interpreters/test_bmi_category_interpreter.py
@@ -0,0 +1,19 @@
+﻿import pytest
+
+from src.core.interpreters.bmi_category_interpreter import BMICategoryInterpreter
+
+
+@pytest.fixture
+def interpreter() -> BMICategoryInterpreter:
+    return BMICategoryInterpreter()
+
+
+@pytest.mark.parametrize("bmi, expected_category", [
+    (17.5, "Underweight"),
+    (18.5, "Normal weight"),
+    (25.0, "Overweight"),
+    (30.0, "Obese")
+])
+def test_interpret_bmi_valid_input_returns_correct_category(interpreter, bmi, expected_category) -> None:
+    category = interpreter.interpret_bmi(bmi)
+    assert category == expected_category
---
+++ tests/models/test_weight.py
@@ -0,0 +1,15 @@
+﻿import pytest
+
+from src.core.enums.unit_type import UnitType
+from src.core.models.height import Height
+
+
+def test_constructor_valid_input_creates_instance():
+    height = Height(1.75, UnitType.Metric)
+    assert height.value == 1.75
+    assert height.unit_type == UnitType.Metric
+
+
+def test_constructor_negative_value_throws_exception():
+    with pytest.raises(ValueError):
+        _ = Height(-1.75, UnitType.Metric)
